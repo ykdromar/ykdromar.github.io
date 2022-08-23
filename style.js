0fd517b5804806a311f759db9908a5ef14872f5b
Yash <ykdromarpro@gmail.com>
2022-08-23 06:41:52 +0530

    added skill bar animation

--- style.js
+++ style.js
@@ -1,7 +1,7 @@
 //smooth scroll
 
 var nav = document.querySelectorAll(".nav a");
-for (var i = 0; i < nav.length; i++) {
+for (let i = 0; i < nav.length; i++) {
   nav[i].addEventListener("click", function (event) {
     event.preventDefault();
     var targetSectioId = this.textContent.trim().toLowerCase();
@@ -23,4 +23,48 @@ for (var i = 0; i < nav.length; i++) {
       window.scrollBy(0, 50);
     }, 20);
   });
+}
+
+// skills bar autofill Animation
+
+var skillBars = document.querySelectorAll(".progress");
+// console.log(skillBars);
+var skillContainer = document.getElementsByClassName("skills-display");
+var isAnimationDone = false;
+window.addEventListener("scroll", checkScroll);
+
+function initialBarWidth() {
+  for (let i = 0; i < skillBars.length; i++) {
+    skillBars[i].style.width = 0 + "%";
+  }
+}
+initialBarWidth();
+function fireAnimation() {
+  for (let bar of skillBars) {
+    let targetWidth = bar.getAttribute("data-bar-width");
+
+    // bar.style.width = targetWidth;
+    // console.log(targetWidth);
+    let currWidth = 0;
+    let widthInterval = setInterval(function () {
+      if (currWidth > targetWidth) {
+        clearInterval(widthInterval);
+        return;
+      }
+      bar.style.width = currWidth + "%";
+      currWidth++;
+    }, 5);
+  }
+}
+
+function checkScroll() {
+  var skillsCoordi = skillContainer[0].getBoundingClientRect();
+  if (!isAnimationDone && skillsCoordi.top <= window.innerHeight) {
+    isAnimationDone = true;
+    // console.log("skill");
+    fireAnimation();
+  } else if (skillsCoordi.top > window.innerHeight) {
+    isAnimationDone = false;
+    initialBarWidth();
+  }
 }
